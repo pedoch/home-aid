91ad9ac907cee865045756aaf64770c132986dc4
Deltanboi <pedroogheneochuko@gmail.com>
2021-03-28 04:12:01 +0100

    set up tailwind properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  purge: ['./pages/**/*.tsx', './styles/**/*.css'],
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
   },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
